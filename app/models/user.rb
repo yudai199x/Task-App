5df021f901dd971920cf8f24ea81d42e23b658cd
EC2 Default User <ec2-user@ip-172-31-35-186.ec2.internal>
2022-10-01 00:32:12 +0000

    Update Gemfile for Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
+  validates :name,  presence: true, length: { maximum: 50 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@ User.create!( name: "Sample User",
               password: "password",
               password_confirmation: "password",
               admin: true )
+
+ User.create!( name: "User1",
+               email: "sample-1@email.com",
+               password: "password",
+               password_confirmation: "password")
